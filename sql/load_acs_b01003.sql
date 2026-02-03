ce8d48d35f8fdf365350bee53bf3a71f5534fa68
dani-loya <dloya@sandiego.edu>
2026-02-02 18:34:42 -0800

    Load ACS B01003 data

--- sql/load_acs_b01003.sql
+++ sql/load_acs_b01003.sql
@@ -1,3 +1,12 @@
 -- load ACS B01003
+USE airbnb_sd;
+
+LOAD DATA LOCAL INFILE 'data/processed/acs_b01003_clean.csv'
+INTO TABLE acs_b01003
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(geo_id, zip_code, year, total_population);
 
 
