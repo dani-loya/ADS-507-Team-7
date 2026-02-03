14484bc34e4ca50722c4d93ce8d290e029b37ad8
dani-loya <dloya@sandiego.edu>
2026-02-02 18:33:09 -0800

    Load acs_dp05 data
    Load data from acs_dp05_clean.csv into acs_dp05 table.

--- sql/load_acs_dp05.sql
+++ sql/load_acs_dp05.sql
@@ -1,2 +1,11 @@
+USE airbnb_sd;
+
+LOAD DATA LOCAL INFILE 'data/processed/acs_dp05_clean.csv'
+INTO TABLE acs_dp05
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(geo_id, zip_code, year, total_population, total_housing_units, median_age);
 -- load ACS B01003 dp05
 
