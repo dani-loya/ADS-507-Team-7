606b1794830d7388f5ccbc1125a9c4bc88fcbe3a
dani-loya <dloya@sandiego.edu>
2026-02-02 18:31:18 -0800

    Load Airbnb listings

--- sql/load_airbnb.sql
+++ sql/load_airbnb.sql
@@ -1,1 +1,12 @@
+USE airbnb_sd;
+
+LOAD DATA LOCAL INFILE 'data/raw/airbnb_listings_sd.csv'
+INTO TABLE airbnb_listings
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(listing_id, name, zipcode, latitude, longitude, room_type, price,
+ minimum_nights, number_of_reviews, last_review, reviews_per_month,
+ calculated_host_listings_count, availability_365);
 
