7dc9fc1e02ac0cd3ecdd35c08969ce273486116d
dani-loya <dloya@sandiego.edu>
2026-02-22 22:00:46 -0800

    Change data source and update column mapping in SQL
    Updated the SQL load command to use the cleaned CSV file and modified the column list to match the new data structure.

--- sql/load_airbnb.sql
+++ sql/load_airbnb.sql
@@ -1,12 +1,11 @@
 USE airbnb_sd;
 
-LOAD DATA LOCAL INFILE 'data/raw/airbnb_raw.csv'
+LOAD DATA INFILE '/var/lib/mysql-files/airbnb_clean.csv'
 INTO TABLE airbnb_listings
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(listing_id, name, zipcode, latitude, longitude, room_type, price,
- minimum_nights, number_of_reviews, last_review, reviews_per_month,
- calculated_host_listings_count, availability_365);
-
+(listing_id, name, host_id, neighbourhood, zip_code, latitude, longitude,
+ room_type, price, minimum_nights, number_of_reviews, last_review,
+ reviews_per_month, availability_365, data_year);
